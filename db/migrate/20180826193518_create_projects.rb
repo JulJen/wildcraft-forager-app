d17aa1ebd74639baf67a5ca8e58a1b3adda527d6
JulJen <juliajen.prog@gmail.com>
2018-09-18 23:11:24 -0400

    Update tables

--- db/migrate/20180826193518_create_projects.rb
+++ db/migrate/20180826193518_create_projects.rb
@@ -5,7 +5,6 @@ class CreateProjects < ActiveRecord::Migration[5.2]
       t.text :description
       t.text :user_comments
       t.integer :team_id
-      t.integer :team_admin_id
       t.boolean :project_admin, default: true
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
