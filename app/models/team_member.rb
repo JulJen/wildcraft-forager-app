5c55846df0b58f34384d794807948716f689b219
JulJen <juliajen.prog@gmail.com>
2018-09-19 03:12:32 -0400

    Update model

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -5,6 +5,8 @@ class TeamMember < ApplicationRecord
   include ActiveModel::Validations
 
   validates :name, presence: true
+  # validates :email, presence: true
+
   # validates :team_id, presence: true
   # validates :project_id, presence: true
   # validates_uniqueness_of :name
