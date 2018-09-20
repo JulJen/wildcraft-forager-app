9608a8fc2c91c67db188e39b1338db3b1cca93ba
JulJen <juliajen.prog@gmail.com>
2018-09-20 14:58:35 -0400

    Update attributes for user create method

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,18 @@ User.destroy_all
     password_digest: Faker::Internet.unique.password, #=> "*%NkOnJsH4"
     email: Faker::Internet.unique.email,  #=> "eliza@mann.net"
     image: Faker::LoremFlickr.image,
-    time_zone: Faker::Address.time_zone
+    time_zone: Faker::Address.unique.time_zone,
+
+    language: Faker::Nation.language,
+    gender: Faker::Gender.type,
+    programlanguage: Faker::ProgrammingLanguage.name,
+    interest: Faker::Educator.subject
   )
 end
 
+
 Team.destroy_all
-20.times do |index|
+30.times do |index|
   Team.create!(name: Faker::Hipster.unique.words(1, true),
     team_admin: false,
     created_at: Faker::Time.between(4.months.ago, 1.month.ago),
@@ -32,7 +38,7 @@ end
 
 
 Project.destroy_all
-20.times do |index|
+30.times do |index|
   Project.create!(name: Faker::Company.unique.bs,
     description: Faker::Hipster.unique.sentences,
     project_admin: false,
