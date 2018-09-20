eb6962eaf38cca252a3e274d98c490615e6ae895
JulJen <juliajen.prog@gmail.com>
2018-09-20 14:57:50 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/404', :to => 'errors#not_found'
-  get '/422', :to => 'errors#unacceptable'
-  get '/500', :to => 'errors#internal_error'
+  get '/404', :to => 'application#not_found'
+  get '/422', :to => 'application#unacceptable'
+  get '/500', :to => 'application#internal_error'
 
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   post '/signin' => 'sessions#create'
   delete '/logout' => 'sessions#destroy', :as => 'logout'
 
-  get '/dashboard/:id/teamboard' => 'team_board#index', :as => 'teamboard'
+  get '/teamboard' => 'team_board#index', :as => 'teamboard'
 
 
 
@@ -41,7 +41,6 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   get '/profile' => 'users#index', :as => 'profile'
   get '/dashboard' => 'users#show'
-
 
   resources :team, only: %i[show] do
     resources :projects, shallow: true
