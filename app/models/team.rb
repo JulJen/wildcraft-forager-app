e3079dfd633c973b020388395b3abef1f9890432
JulJen <juliajen.prog@gmail.com>
2018-09-20 13:29:38 -0400

    Update validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -13,7 +13,14 @@ class Team < ApplicationRecord
   include ActiveModel::Validations
 
   validates :name, presence: true
-  validates_uniqueness_of :name
 
+  validate :ensure_unique, on: :create
+
+  # validates :team_admin, inclusion: { in: [ true, 'true' ] }
+
+  # validates_inclusion_of :team_admin, :in => [true, false]
+  # validates :team_admin, :presence => { :message => "Please select some formats!" }
+  # validates :team_admin, acceptance: {accept: true} , on: :create, allow_nil: false
+  validates :team_admin, :inclusion => {:in => [true, false]}, presence: true
 
 end
