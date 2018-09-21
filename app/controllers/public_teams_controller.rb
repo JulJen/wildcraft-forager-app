8fd8e20c24ab8f2a29ad258183ad5fecce6e74fc
JulJen <juliajen.prog@gmail.com>
2018-09-21 16:34:15 -0400

    Update controller name

--- app/controllers/public_teams_controller.rb
+++ app/controllers/public_teams_controller.rb
@@ -1,8 +1,9 @@
-class TeamBoardController < ApplicationController
+class PublicTeamsController < ApplicationController
   before_action :require_logged_in
 
   def index
     @teams = Team.all
+    @projects = Project.all
 
     @current_teams = current_user.teams
     @current_projects = current_user.projects
--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -2,6 +2,9 @@ class TeamMembersController < ApplicationController
   before_action :require_logged_in
 
   def index
+binding.pry
+    @users = User.all
+
     @team_member = TeamMember.find_by_id(params[:id])
 
     @team = Team.find_by(user_id: current_user)
@@ -10,13 +13,19 @@ class TeamMembersController < ApplicationController
       @current_members = @project.team_members
     end
 
-    @member_deleted_message  = session[:member_delete]
+    @member_success_message = session[:member_success]
+    session[:member_success] = nil
+
+    @member_delete_message = session[:member_delete]
     session[:member_delete] = nil
   end
 
   def new
+    @team_members = TeamMember.all
     @users = User.all
+
     @team_member = TeamMember.new
+
     @team = Team.find_by(user_id: current_user)
     @project = Project.find_by_id(params[:project_id])
   end
@@ -24,28 +33,39 @@ class TeamMembersController < ApplicationController
   def create
     @team = Team.find_by(user_id: current_user)
     @project = Project.find_by_id(params[:project_id])
-    @team_member= TeamMember.new(member_params)
-binding.pry
-    if @project.project_admin == true
-      if @team_member.save
-        @project.team_members << @team_member
+    @user = User.find_by_id(member_params[:user_id])
 
-        session[:member_success] = "Team member added!"
-        redirect_to project_team_members_path(@project)
+    if @project.project_admin == true
+      if @user.id == member_params[:user_id].to_i
+        @team_member = TeamMember.new(member_params)
+        if @team_member.save
+          @project.team_members << @team_member
+
+          session[:member_success] = "Team member added!"
+          redirect_to project_team_members_path(@project)
+        end
       end
     else
-      render :new
+      session[:admin_failure] = "Failure to invite members, contact the project admin."
+      render :show
     end
   end
 
   def show
     @team = Team.find_by(user_id: current_user)
     @project = Project.find_by_id(params[:project_id])
+
+    @users = User.all
     @team_member = TeamMember.find_by_id(params[:id])
+
     @current_members = @project.team_members
+
+    @member_deleted_message  = session[:member_delete]
+    session[:member_delete] = nil
   end
 
   def destroy
+binding.pry
     @team = Team.find_by(user_id: current_user)
     @project = Project.find_by_id(params[:project_id])
     @team_member = TeamMember.find_by_id(params[:id])
@@ -60,7 +80,7 @@ binding.pry
   private
 
   def member_params
-    params.require(:team_member).permit(:name, :email, :image)
+    params.require(:team_member).permit(:user_id)
   end
 
 end
