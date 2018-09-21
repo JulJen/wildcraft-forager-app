7e6747b6479524652547fd23e216a161562cd88a
JulJen <juliajen.prog@gmail.com>
2018-09-21 16:32:04 -0400

    Update tables

--- db/migrate/20180918231827_create_team_members.rb
+++ db/migrate/20180918231827_create_team_members.rb
@@ -1,12 +1,8 @@
 class CreateTeamMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :team_members do |t|
-      t.string :name
-      t.string :email
-      t.integer :user_id
-      t.integer :team_id
+      t.string :user_id
       t.integer :project_id
-      t.text :image
 
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
--- db/migrate/20180920193931_add_team_member_info_to_team_member.rb
+++
@@ -1,9 +0,0 @@
-class AddTeamMemberInfoToTeamMember < ActiveRecord::Migration[5.2]
-  def change
-    add_column :team_members, :time_zone, :string
-    add_column :team_members, :language, :string
-    add_column :team_members, :gender, :string
-    add_column :team_members, :programlanguage, :string
-    add_column :team_members, :interest, :string
-  end
-end
